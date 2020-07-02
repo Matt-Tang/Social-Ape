e2aec5a7d9b179fdf4679ab6b748360e0781175b
Matthew Tang <matthewtang1999@gmail.com>
2020-07-02 16:46:27 -0400

    Added the sign up feature using email and authentication

--- functions/index.js
+++ functions/index.js
@@ -9,10 +9,27 @@ const app = express()
 
 admin.initializeApp(); //firebase object for accessing database
 
+// Firebase Authentication
+const config = {
+    apiKey: "AIzaSyBVrfKKc36qH3VkODtLsNCS7vQ2RoYzQXc",
+    authDomain: "socialape-a3677.firebaseapp.com",
+    databaseURL: "https://socialape-a3677.firebaseio.com",
+    projectId: "socialape-a3677",
+    storageBucket: "socialape-a3677.appspot.com",
+    messagingSenderId: "203550461356",
+    appId: "1:203550461356:web:c04aedbcce3e90834c7e09",
+    measurementId: "G-TB9NS84TG4"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
+
+
 // First param is the route, second param is the handler
 app.get('/screams', (req, res) => {
-    admin.firestore()
-        .collection('screams')
+    db.collection('screams')
         .orderBy('createdAt', 'desc')
         .get()
         .then((data) => {
@@ -37,8 +54,7 @@ app.post('/scream', (req, res) => {
         createdAt: new Date().toISOString()
     };
 
-    admin.firestore()
-        .collection('screams')
+    db.collection('screams')
         .add(newScream)
         .then((doc) => {
             res.json({ message: `document ${doc.id} created successfully!`})
@@ -50,6 +66,55 @@ app.post('/scream', (req, res) => {
 
 
 });
+
+/* Sign up route */
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+    // Check if user handle exists or not, you will get a doc regardless if it exists or not
+    let token, userId;
+    db.doc(`/users/${newUser.handle}`).get()
+        .then((doc) => {
+            if(doc.exists){ // handle exist we can not create it
+                return res.status(400).json({ handle: 'this handle is already taken'});
+            } else { // else it is unique and we should create it
+                return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
+            }
+        })
+        .then((data) => { // successfully created handle so get token
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then((idToken) => {
+            token = idToken;
+            const userCredentials = { 
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId: userId
+            };
+
+            // Create user in database
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+        .then(() =>{
+            return res.status(201).json({ token });
+        })
+        .catch((err) => {
+            console.error(err);
+            if(err.code === 'auth/email-already-in-use'){
+                return res.status(400).json({ error: 'Email is already in use' });
+            }
+            else {
+                return res.status(500).json({ error: err.code });
+            }
+        });
+})
 
 // To expose our endpoint to be something like https://baseurl/api/function
 // app is the container for all our routes so we just expose that, this is done through express
